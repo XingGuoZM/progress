38389f2738ea9f2fc52c793bb9790eb5c4f151d3
xingguozm <18969176324@163.com>
2022-09-26 09:41:56 +0000

    Update 11 files

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import useStageProgress from "../../hooks/useStageProgress";
-import { IStageProgress } from "../../types";
 import "./index.css";
 
-export default function Progress(props: IStageProgress) {
-  const { stageList } = props;
-  const { percentList } = useStageProgress(props);
-  const rangeList = new Array(stageList.length).fill(100);
+export default function Progress(props) {
+  const { item, percent } = props;
+
   return (
-    <div className="progress-wrap">
-      <div style={{ display: "flex" }}>
-        {rangeList.map((item, index) => (
-          <div key={index} className="progress" style={{ width: `${item}px` }}>
-            <div
-              className="progress-bar"
-              style={{
-                transform: `translateX(${percentList[index] * 100 - 100}%)`
-              }}
-            />
-          </div>
-        ))}
-      </div>
+    <div className="progress" style={{ width: `${item}px` }}>
+      <div
+        className="progress-bar"
+        style={{
+          transform: `translateX(${percent * 100 - 100}%)`
+        }}
+      />
     </div>
   );
 }
